3d6f8796bf5f8ca3350d3f9b0dfea69be0e62380
violeren <65766026+violeren@users.noreply.github.com>
2020-05-22 19:55:11 +0700

    Update run.php

--- run.php
+++ run.php
@@ -58,7 +58,7 @@ function create_iflix() {
 		'Cookie: build-release=stable; _u=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NDQsInZlcnNpb24iOjIsImEiOjQ3MjA4MTk3NCwicCI6NDcxNjQ3MDQ1LCJlIjo1NTA1MjQ2MTQsImkiOiIiLCJpZGVudGl0eVR5cGUiOiJ2aXNpdG9yIiwiaTRhIjp0cnVlLCJmbCI6ZmFsc2UsImdsb2JhbCI6ZmFsc2UsImF1dGhvcml6YXRpb24iOiJCZWFyZXIgMGNmODczMzQtYzk5NC00ZWIwLTg4ZjItMDJlYWNmYzAzOTY4Iiwic2lkIjoiNmFjMmUxODdlMzRjM2VjMmZkODE2ZjFmOTBiMmI4M2IxNzgyZDQ2N35BOENEQURFMkU0QTdGOEM2QTU0RTQ5QzM3NjJDNkE2NCIsIm5hbWUiOiIiLCJ2aWQiOjQ0LCJ1bmlxdWVJRCI6IjU0MzUwOWU2LWMwN2ItNGQzMi05ZWQyLWNkNzFjZTViZTgxNy1peGRqIiwibWF4UmF0aW5nIjoiYWR1bHRzIiwibGFuZyI6ImVuX1VTIiwidmlzaXRvciI6dHJ1ZSwiZUlhdCI6MTU5MDA4NjQ5MSwiYUlhdCI6MTU5MDA4NjYzMCwiZXhwIjoxNTkyNTA1ODMwLCJpYXQiOjE1OTAwODY2MzAsImlzcyI6Imh0dHBzOi8vaWQuaWZsaXguY29tIn0.gZICBR32CfPwE87RTEPSk_uD8IM2kUNOP4pJusYHYqA; lux_uid=15900864'.number(10).'; _ga=GA1.2.794169197.1590086494; _ga_XEVBJ0MPS7=GS1.1.1590086493.1.1.1590086635.0; _gcl_au=1.1.1079203672.1590086496; _gid=GA1.2.1717001541.1590086496; _fbp=fb.1.1590086500634.523061093; afUserId=430f6b89-577a-4787-9c6d-48311a345c18-p; _gat_UA-60124943-7=1'
 	];
 
-	$register = post('https://www.iflix.com/api/identity/v4/register', '{"deviceId":"543509e6-c07b-4d32-9ed2-cd71ce5be817-ixdj","deviceName":"Win32","name":"'.$name[1].'","email":"'.$email[1].'","password":"yudhagans"}', $headers);
+	$register = post('https://www.iflix.com/api/identity/v4/register', '{"deviceId":"543509e6-c07b-4d32-9ed2-cd71ce5be817-ixdj","deviceName":"Win32","name":"'.$name[1].'","email":"'.$email[1].'","password":"rengans"}', $headers);
 
 	if (strripos($register, '"account"')) {
 		echo $data = "[+] Success create account | ".$email[1]." | rengans\n\r";
